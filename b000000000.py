d7562365e77ceef07c187d9ec0cd0c1f0fb84b23
hasna713 <hasnamohamed@protonmail.com>
2021-02-02 18:34:01 +0200

    La taille du document lu est <mon chiffre ICI>

--- b000000000.py
+++ b000000000.py
@@ -16,13 +16,35 @@ session = mysqlx.get_session({
 
 db = session.get_schema("wold_x")
 
+def lecture(fichier):
+      nomColl = "temp"
+      maColl = db.create_collection(nomColl)
+      json = charge(fishier)
+      maColl.add(json).execute()
+      docs = maColl.find().execute()
+      db.drop_collection(nomColl)
+      return(docs)
+
 def charge(fichier):
    with open(fichier) as f:
       return json.load(f)
 
+def former_des_chefs(docs):
+  nomColl = 'chefs_de_governement'
+  maColl = db.create_collection(nomColl)
+
+
+for doc in docs.fetch_all():
+  for country in doc.countries:
+    maColl.add(country['governement']).execute)
+
+docs = maColl.find().execute()
+db.drop_collection(nomColl)
+
 def main():
-  print(charge('b000000000.json'))
+  docs = lecture('b000000000.json')
+  chefs = former_des_chefs(docs)
+  print(len(chefs.fetch_all()))
   session.close
 
-if __name__ == "__main__":
-    main()
+return(docs)
