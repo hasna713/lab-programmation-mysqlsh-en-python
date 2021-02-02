91e57c72ade1225ee2a3ac2b7c7a6ed1e8be428e
hasna713 <hasnamohamed@protonmail.com>
2021-02-02 15:39:41 +0200

    La taille du document lu est <mon chiffre ICI>

--- b000000000.py
+++ b000000000.py
@@ -5,6 +5,16 @@
 """
 
 import json
+import mysqlx
+
+session = mysqlx.get_session({
+  "host": "localhost",
+  "port": 33060,
+  "user": "root",
+  "password": "password"
+})
+
+db = session.get_schema("wold_x")
 
 def charge(fichier):
    with open(fichier) as f:
@@ -12,6 +22,7 @@ def charge(fichier):
 
 def main():
   print(charge('b000000000.json'))
+  session.close
 
 if __name__ == "__main__":
     main()
